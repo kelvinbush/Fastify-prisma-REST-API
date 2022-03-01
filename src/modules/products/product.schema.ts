b1649161f12330e5a2fb43925c5b55d4d28b72a9
Kelvin Bush <kelybush@gmail.com>
2022-03-01 14:48:59 +0300

    setting up product schemas
    Signed-off-by: Kelvin Bush <kelybush@gmail.com>

---
+++ src/modules/products/product.schema.ts
@@ -0,0 +1,31 @@
+import { z } from "zod";
+import { buildJsonSchemas } from "fastify-zod";
+
+const productInput = {
+  title: z.string(),
+  price: z.string(),
+  content: z.string().optional(),
+};
+
+const productGenerated = {
+  id: z.number(),
+  createdAt: z.string(),
+  updatedAt: z.string(),
+};
+
+const createProductSchema = z.object({
+  ...productInput,
+});
+
+const productResponseSchema = z.object({
+  ...productInput,
+  ...productGenerated,
+});
+const productsResponseSchema = z.array(productResponseSchema);
+
+export type CreateProductInput = z.infer<typeof createProductSchema>;
+export const { schemas: productSchemas, $ref } = buildJsonSchemas({
+  createProductSchema,
+  productResponseSchema,
+  productsResponseSchema,
+});
